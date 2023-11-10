6f60a1ca640fbb915121b5c454067853f47e2fa9
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2023-11-10 17:38:15 -0300

    creating rds with terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,16 @@
+# provider.tf
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.21.0"
+    }
+  }
+}
--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -1,15 +1,35 @@
-# main.tf
-provider "aws" {
-  region = "us-east-1"  # Cambia a tu región preferida
-}
+# rds/main.tf
 
-resource "aws_db_instance" "mydb" {
-  identifier           = "mydb-instance"
+resource "aws_db_instance" "postgres_instance" {
+  identifier            = "my-postgres-db"
   allocated_storage    = 20
   engine               = "postgres"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  username             = "admin"
-  password             = "your_password"  # Cambia a tu contraseña
-  parameter_group_name = "default.postgres11"
+  engine_version       = "15.4"
+  instance_class       = "db.t3.micro"  # Cambia a una clase de instancia compatible
+  username             = var.db_username
+  password             = var.db_password
+  vpc_security_group_ids = [var.aws_security_group_id]
+  skip_final_snapshot  = true
+  multi_az             = true
+  publicly_accessible  = false
+  db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
+
+  tags = {
+    Name = "MyPostgresDB"
+  }
+}
+
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres_instance.endpoint
+}
+
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  name       = "my-db-subnet-group"
+  subnet_ids = [
+    var.aws_subnet_a_private_id,
+    var.aws_subnet_b_private_id,
+    var.aws_subnet_c_private_id,
+    var.aws_subnet_d_private_id,
+  ]
 }
